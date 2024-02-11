e847c6860332c8317e48c5d941aad375a8bcc131
ccx1010sky <ccx1010@gmail.com>
2024-02-11 23:19:38 +0000

    create functionUrl function

--- lib/docker-lambda-aws-stack.ts
+++ lib/docker-lambda-aws-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { HttpMethod } from 'aws-cdk-lib/aws-events';
 
 
 export class DockerLambdaAwsStack extends cdk.Stack {
@@ -12,6 +13,15 @@ export class DockerLambdaAwsStack extends cdk.Stack {
       memorySize:1024,
       timeout: cdk.Duration.seconds(10),
       architecture: lambda.Architecture.X86_64
+    })
+
+    const functionUrl = dockerFunc.addFunctionUrl({
+      authType:lambda.FunctionUrlAuthType.NONE,
+      cors:{
+        allowedMethods:[lambda.HttpMethod.ALL],
+        allowedHeaders:['*'],
+        allowedOrigins:["*"],
+      }
     })
   }
 }
