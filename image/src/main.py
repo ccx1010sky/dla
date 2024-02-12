af3677e9400f9300fcea3336c46b08b8eed41b9f
ccx1010sky <ccx1010@gmail.com>
2024-02-12 17:02:54 +0000

    minor modificatin

--- image/src/main.py
+++ image/src/main.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 
-def handler(event,context):
-    arr = np.random.randint(0,10,(3,3))
+def handler(event, context):
+    arr = np.random.randint(0, 10, (3, 3))
     return {
-        "statusCode":200,
-        "body": {"message": "hello world!", "array": arr.tolist()}
+        "statusCode": 200,
+        "body": {"message": "Hello from Lambda!", "array": arr.tolist()},
     }
